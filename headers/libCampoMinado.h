5cd759f48abd1a2501a2e40b54bee7fa371eea61
Igor Benedet <benedetigor@gmail.com>
2020-09-28 19:25:25 -0300

    assinatura das funções

--- headers/libCampoMinado.h
+++ headers/libCampoMinado.h
@@ -3,6 +3,17 @@
 
 #include "./Tile.h";
 
-    void showField( Tile** field);
+    void generateField();
+
+    void generateBombs();
+
+    void showField( Tile** field );//recebe matriz de tiles
+
+    void showRevealedField( Tile** field );
+
+    bool checkGameOver(); //se usuário clicou em uma bomba
+
+    bool checkVictory(); //se usuário revelou todas as posições sem bomba
+
 
 #endif
